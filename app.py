be0282adb63974fa159b858fdbbb55447e652f28
davidjeffers <ohsjeffers@gmail.com>
2022-03-24 17:14:33 -0500

    fix

--- app.py
+++ app.py
@@ -13,32 +13,43 @@ RESPONSES = []
 @app.get("/")
 def render_survey_start():
     """Displays survey title and start on the page"""
-    return render_template("survey_start.html", title=survey.title, instructions=survey.instructions )
+
+    return render_template(
+        "survey_start.html",
+        title=survey.title,
+        instructions=survey.instructions)
+
 
 @app.post("/begin")
 def redirect_question_one():
     """Redirect to first question"""
-    
+
     return redirect("/question/0")
+
 
 @app.get("/question/<int:index>")
 def render_question(index):
     """Displays current question and input"""
+
     if len(RESPONSES) == len(survey.questions):
         return redirect("/completion")
     else:
         question = survey.questions[index]
         return render_template("question.html", question=question)
 
+
 @app.post("/answer")
 def redirect_answer():
     """Redirect to next question store answer in RESPONSES"""
+
     RESPONSES.append(request.form["answer"])
     next_question = len(RESPONSES)
     return redirect(F"/question/{next_question}")
 
+
 @app.get("/completion")
 def render_completion_page():
     """Displays completion of survey"""
+    
     # print(RESPONSES)
     return render_template("/completion.html")
